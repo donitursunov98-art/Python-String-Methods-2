5ba69a2586766d25d58b59b7211b02e59fb4bc60
Abdujalol <Donitursunov98@gmail.com>
2025-12-12 22:41:17 +0500

    17-mashq

--- task17.py
+++ task17.py
@@ -1,1 +1,24 @@
-d
+password = input('password: ')
+
+if password.find('0') != -1:
+    print(True)
+elif password.find('1') != -1:
+    print(True)
+elif password.find('2') != -1:
+    print(True)
+elif password.find('3') != -1:
+    print(True)
+elif password.find('4') != -1:
+    print(True)
+elif password.find('5') != -1:
+    print(True)
+elif password.find('6') != -1:
+    print(True)
+elif password.find('7') != -1:
+    print(True)
+elif password.find('8') != -1:
+    print(True)
+elif password.find('9') != -1:
+    print(True)
+else:
+    print(False)
